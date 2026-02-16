e4354d19c783e572d80013f1f3437c96a4834e84
priscilla <gen2priscilla@gmail.com>
2026-02-17 03:59:41 +0900

    Create master.js

---
+++ master.js
@@ -0,0 +1,10 @@
+function hum() {
+
+    let a = document.getElementById("fff")
+    a.classList.toggle("as")
+
+    let g = document.getElementById("fff")
+    g.onclick = () => {
+        g.classList.toggle("as")
+    }
+}
