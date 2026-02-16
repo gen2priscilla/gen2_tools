62c1683cb7b0248ef9368416d1acee688da9e2fd
priscilla <gen2priscilla@gmail.com>
2026-02-17 01:39:28 +0900

    update

--- stutas_ref/status_ref.js
+++ stutas_ref/status_ref.js
@@ -1,5 +1,20 @@
 function hum() {
-    let bu = document.createElement("a")
-    bu.href = "/gen2_tools/index.html"
-    bu.click()
+    // let bu = document.createElement("a")
+    // bu.href = "/gen2_tools/index.html"
+    // bu.click()
+
+    let a = document.getElementById("fff")
+    a.classList.toggle("as")
+
+    let g = document.getElementById("fff")
+    g.onclick = () => {
+        g.classList.toggle("as")
+    }
+}
+
+window.onload = () => {
+    let s = document.getElementById("fff")
+    let ff = document.getElementById("qwer")
+
+    console.log(s.parentNode)
 }
